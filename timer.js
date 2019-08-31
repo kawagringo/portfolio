635e15ccb48ace29e2b84441b988aed20b64d1f1
Lukasz Kawecki <52894480+kawagringo@users.noreply.github.com>
2019-08-31 11:48:46 +0100

    Add files via upload

--- timer.js
+++ timer.js
@@ -1,21 +1,22 @@
 function counting()
-	{
-		var dzisiaj = new Date();
+    {
 
-		var dzien = dzisiaj.getDate();
-		var miesiac = dzisiaj.getMonth()+1;
-		var rok = dzisiaj.getFullYear();
-		
-		var godzina = dzisiaj.getHours();
-		if (godzina<10) godzina = "0"+godzina;
-		
-		var minuta = dzisiaj.getMinutes();
-		if (minuta<10) minuta = "0"+minuta;
-		
-		var sekunda = dzisiaj.getSeconds();
-		if (sekunda<10) sekunda = "0"+sekunda;
-		
-		document.getElementById("clock").innerHTML = 
-		dzien+"/"+miesiac+"/"+rok+" | "+godzina+":"+minuta+":"+sekunda;
-		 setTimeout("counting()",1000);
-	}
+        var today = new Date();
+
+        var day = today.getDate();
+        var month = today.getMonth()+1;
+        var year = today.getFullYear();
+
+        var hour = today.getHours();
+        if (hour<10) hour = "0"+hour;
+        var minute = today.getMinutes();
+        if (minute<10) minute = "0"+minute;
+        var second = today.getSeconds();
+        if (second<10) second = "0"+second;
+
+
+        document.getElementById("clock").innerHTML = day+"/"+month+"/"+year+"|"+hour+":"+minute+":"+second;
+
+        setTimeout("counting()",1000);
+    
+    }
